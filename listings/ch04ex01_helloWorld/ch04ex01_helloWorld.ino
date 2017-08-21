7233cf451ae9253709f2f2ea098b4c4c219d0cad
cmgrass <C.grass@live.com>
2017-08-21 08:47:00 -0700

    Added listing 4.1

---
+++ listings/ch04ex01_helloWorld/ch04ex01_helloWorld.ino
@@ -0,0 +1,10 @@
+#define DEBUG_OUTPUT_MESSAGE_MAX_LENGTH 80  // Create a macro for our constant
+
+void setup()
+{
+  char message[DEBUG_OUTPUT_MESSAGE_MAX_LENGTH);  // This byte array will contain our message string
+
+  snprintf(); // 
+  
+}
+
