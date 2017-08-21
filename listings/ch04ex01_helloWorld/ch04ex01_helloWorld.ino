13fc84eec6b95599c89aca55d5d6ca9190f7a45d
cmgrass <C.grass@live.com>
2017-08-21 15:37:23 -0700

    Finished listing 4.1.

--- listings/ch04ex01_helloWorld/ch04ex01_helloWorld.ino
+++ listings/ch04ex01_helloWorld/ch04ex01_helloWorld.ino
@@ -2,9 +2,14 @@
 
 void setup()
 {
-  char message[DEBUG_OUTPUT_MESSAGE_MAX_LENGTH);  // This byte array will contain our message string
+  char message[DEBUG_OUTPUT_MESSAGE_MAX_LENGTH];  // This byte array will contain our message string
 
-  snprintf(); // 
-  
+  snprintf(message, DEBUG_OUTPUT_MESSAGE_MAX_LENGTH, "Hello, world!\n"); // same as `printf()`, except prints formatted string to memory buffer instead of standard output port.
+
+  Serial.begin(9600);
+  Serial.write(message);
 }
 
+void loop()
+{
+}
