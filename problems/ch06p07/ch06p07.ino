578d88704ad57a912bc4f2ef20c1d0eabe53bd2b
cmgrass <C.grass@live.com>
2017-08-27 16:14:59 -0700

    finalized program which cycles between: [motor: CW, OFF, CCW, OFF], based on debounced button input.

--- problems/ch06p07/ch06p07.ino
+++ problems/ch06p07/ch06p07.ino
@@ -1,3 +1,6 @@
+void PinStateOns(unsigned char *inputPort, unsigned char *motorState, unsigned char *prevSample, unsigned char *currSample, unsigned char mask);
+void MotorControl(unsigned char *inputPort, unsigned char *motorState);
+void DebounceInput();
 void MyDelay(unsigned long milliSeconds);
 #define MASK_PORTD_INPUT 0x01
 #define MASK_PORTD_OUTPUT 0x0E  // NOT this for motor disable
@@ -7,8 +10,7 @@ unsigned char *portDataD;
 unsigned char *portPinD;
 
 // Debugging
-#define MESSAGE_MAX_LEN 80
-char message[MESSAGE_MAX_LEN];
+//#define MESSAGE_MAX_LEN 80
 
 void setup()
 {
@@ -22,29 +24,85 @@ void setup()
   *portDDRD |= MASK_PORTD_OUTPUT; // Define outputs
   *portDataD |= MASK_PORTD_INPUT; // Enable internal pullup resistor for input
 
-  Serial.begin(9600);
+  //Serial.begin(9600);
 }
 
 void loop()
+{  
+  unsigned char previousSample;
+  unsigned char currentSample;
+  unsigned char myMotorState;
+  unsigned char *prevSampleAddr;
+  unsigned char *currSampleAddr;
+  unsigned char *motorStateAddr;
+  
+  prevSampleAddr = &previousSample;
+  currSampleAddr = &currentSample;
+  motorStateAddr = &myMotorState;
+  
+  PinStateOns(portPinD, motorStateAddr, prevSampleAddr, currSampleAddr, MASK_PORTD_INPUT);
+
+}
+
+void PinStateOns(unsigned char *inputPort, unsigned char *motorState, unsigned char *prevSample, unsigned char *currSample, unsigned char mask)
 {
-  *portDataD &= (~MASK_PORTD_OUTPUT);  // Disable motor
-  *portDataD |= MASK_MOTOR_DIR01;
-  snprintf(message, MESSAGE_MAX_LEN, "%#x\n", *portDataD);
-  Serial.write(message);
-  MyDelay(2000);
-  *portDataD &= (~MASK_PORTD_OUTPUT);
-  snprintf(message, MESSAGE_MAX_LEN, "%#x\n", *portDataD);
-  Serial.write(message);
-  MyDelay(2000);
-  *portDataD &= (~MASK_PORTD_OUTPUT);
-  *portDataD |= MASK_MOTOR_DIR02;
-  snprintf(message, MESSAGE_MAX_LEN, "%#x\n", *portDataD);
-  Serial.write(message);
-  MyDelay(2000);
-  *portDataD &= (~MASK_PORTD_OUTPUT);
-  snprintf(message, MESSAGE_MAX_LEN, "%#x\n", *portDataD);
-  Serial.write(message);
-  MyDelay(2000);
+  /* One shot pin state change*/
+  
+  unsigned char confirmSample;
+  //char message[MESSAGE_MAX_LEN];
+
+  *currSample = (*inputPort) & mask;
+
+  if (*currSample != *prevSample)
+  {
+    DebounceInput();
+    confirmSample = (*inputPort) & mask;
+
+    if (confirmSample == *currSample)
+    {
+      MotorControl(inputPort, motorState);
+      //snprintf(message, MESSAGE_MAX_LEN, "Change State: %#x\n", *motorState);
+      //Serial.write(message);
+    }
+  }
+  *prevSample = *currSample;
+}
+
+void MotorControl(unsigned char *inputPort, unsigned char *motorState)
+{
+  switch (*motorState)
+  {
+    case (0x01):
+      *inputPort &= (~MASK_PORTD_OUTPUT);
+      *inputPort |= MASK_MOTOR_DIR01;
+      *motorState = 0x02;
+      break;
+
+    case (0x02):
+      *inputPort &= (~MASK_PORTD_OUTPUT);
+      *motorState = 0x03;
+      break;
+
+    case (0x03):
+      *inputPort &= (~MASK_PORTD_OUTPUT);
+      *inputPort |= MASK_MOTOR_DIR02;
+      *motorState = 0x04;
+      break;
+
+    case (0x04):
+      *inputPort &= (~MASK_PORTD_OUTPUT);
+      *motorState = 0x01;
+
+    default:
+      *inputPort &= (~MASK_PORTD_OUTPUT);
+      *motorState = 0x01;
+      break;
+  }
+}
+
+void DebounceInput()
+{
+  MyDelay(10);
 }
 
 void MyDelay(unsigned long milliSeconds)
